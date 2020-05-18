ca632bd87382e4515016fe1ce4d891c8889b8f86
Mate Iszlai <iszimate@gmail.com>
2020-05-18 13:28:47 +0200

    Create Dog class

---
+++ isp/isp/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isp
+{
+    public class Dog : IAnimal
+    {
+        public void fly()
+        {
+
+        }
+
+        public  void run()
+        {
+            Console.WriteLine("Dog is running");
+        }
+
+        public void bark()
+        {
+            Console.WriteLine("Dog is barking");
+        }
+    }
+}
