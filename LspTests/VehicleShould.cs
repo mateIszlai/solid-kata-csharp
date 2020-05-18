efab717a04b01dbbf4c1999b8926427c27896c16
Mate Iszlai <iszimate@gmail.com>
2020-05-18 12:18:13 +0200

    Create tests to Vehicle

---
+++ LspTests/VehicleShould.cs
@@ -0,0 +1,44 @@
+﻿using lsp;
+using NUnit.Framework;
+
+namespace LspTests
+{
+    [TestFixture]
+    public class VehicleShould
+    {
+        [Test]
+        public void start_engine()
+        {
+            Vehicle vehicle = new TestableVehicle();
+            vehicle.startEngine();
+
+            Assert.IsTrue(vehicle.engineIsStarted());
+        }
+
+        [Test]
+        public void stop_engine()
+        {
+            Vehicle vehicle = new TestableVehicle();
+
+            vehicle.startEngine();
+            vehicle.stopEngine();
+
+            Assert.IsFalse(vehicle.engineIsStarted());
+        }
+
+
+        public class TestableVehicle : Vehicle
+        {
+
+            public override void fillUpWithFuel()
+            {
+
+            }
+
+            public override void chargeBattery()
+            {
+
+            }
+        }
+    }
+}
