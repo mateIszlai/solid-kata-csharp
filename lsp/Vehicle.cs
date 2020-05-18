a03290cfbb08e0cca1834c83c24099d5e80d72a1
Mate Iszlai <iszimate@gmail.com>
2020-05-18 11:22:41 +0200

    Create lsp project

---
+++ lsp/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿namespace lsp
+{
+    public abstract class Vehicle
+    {
+        private bool engineStarted = false;
+
+        public void startEngine()
+        {
+            this.engineStarted = true;
+        }
+
+        public bool engineIsStarted()
+        {
+            return engineStarted;
+        }
+
+        public void stopEngine()
+        {
+            this.engineStarted = false;
+        }
+
+        public abstract void fillUpWithFuel();
+
+        public abstract void chargeBattery();
+    }
+}
