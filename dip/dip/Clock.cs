5f99e35452c52a89f72b247e1c295449a9323786
Mate Iszlai <iszimate@gmail.com>
2020-05-18 16:27:16 +0200

    Create Clock class

---
+++ dip/dip/Clock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dip
+{
+    public class Clock
+    {
+        public MonthDay MonthDay
+        {
+            get
+            {
+                var date = DateTime.Today;
+                return new MonthDay { Day = date.Day, Month = date.Month };
+            }
+        }
+    }
+}
