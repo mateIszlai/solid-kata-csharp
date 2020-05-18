a2b9d93c5bc082d2878976f3d9cb2e95da511e7d
Mate Iszlai <iszimate@gmail.com>
2020-05-18 16:29:58 +0200

    Create Email class

---
+++ dip/dip/Email.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dip
+{
+    public class Email
+    {
+        public string To { get; private set; }
+        public string Subject { get; private set; }
+        public string Message { get; private set; }
+
+        Email(string to, string subject, string message)
+        {
+            To = to;
+            Subject = subject;
+            Message = message;
+        }
+    }
+}
