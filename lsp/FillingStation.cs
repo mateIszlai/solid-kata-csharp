6e605477d33fc6492ec6b4cae00b3cf0ea70eaaa
Mate Iszlai <iszimate@gmail.com>
2020-05-18 11:49:36 +0200

    Create FillingStation

---
+++ lsp/FillingStation.cs
@@ -0,0 +1,19 @@
+﻿namespace lsp
+{
+    public class FillingStation
+    {
+        public void refuel(Vehicle vehicle)
+        {
+            if (typeof(PetrolCar).IsInstanceOfType(vehicle)) {
+                vehicle.fillUpWithFuel();
+            }
+        }
+
+        public void charge(Vehicle vehicle)
+        {
+            if (typeof(ElectricCar).IsInstanceOfType(vehicle)) {
+                vehicle.chargeBattery();
+            }
+        }
+    }
+}
