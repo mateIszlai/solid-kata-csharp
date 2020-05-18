bbe933f653ffcf9ab2da724033aa52bb94ffdbb1
Mate Iszlai <iszimate@gmail.com>
2020-05-18 13:18:34 +0200

    Create Bird class

---
+++ isp/isp/Bird.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isp
+{
+    public class Bird : IAnimal
+    {
+        public void bark() { }
+        public void run()
+        {
+            Console.WriteLine("Bird is running");
+        }
+        public void fly()
+        {
+            Console.WriteLine("Bird is flying");
+        }
+    }
+}
