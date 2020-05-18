2233bfcac0d12ab57e180680f632642dbbe84c25
Mate Iszlai <iszimate@gmail.com>
2020-05-18 11:46:58 +0200

    Create PetrolCar

---
+++ lsp/PetrolCar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lsp
+{
+    public class PetrolCar : Vehicle
+    {
+        private const int FUEL_TANK_FULL = 100;
+        public int FuelTankLevel { get; private set; }
+
+
+        public override void fillUpWithFuel()
+        {
+            FuelTankLevel = FUEL_TANK_FULL;
+        }
+
+        public override void chargeBattery()
+        {
+            throw new InvalidOperationException("A petrol car cannot be recharged");
+        }
+    }
+}
