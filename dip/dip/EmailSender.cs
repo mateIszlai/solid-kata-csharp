ed6e51b936c366e0f708490146297d9aa42d2167
Mate Iszlai <iszimate@gmail.com>
2020-05-18 16:34:08 +0200

    Create emailSender

---
+++ dip/dip/EmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dip
+{
+    public class EmailSender
+    {
+        public void send(Email email)
+        {
+            Console.WriteLine($"To:{email.To}, Subject: {email.Subject}, Message: {email.Message}");
+        }
+    }
+}
