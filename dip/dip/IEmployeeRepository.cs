4ff6549258bd05822a3d05eb0f76d87397b13998
Mate Iszlai <iszimate@gmail.com>
2020-05-18 16:22:40 +0200

    Create IEmployeeRepository interface

---
+++ dip/dip/IEmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dip
+{
+    public interface IEmployeeRepository
+    {
+        List<Employee> findEmployeesBornOn(MonthDay monthDay);
+    }
+
+    public struct MonthDay : IEquatable<MonthDay>
+    {
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public bool Equals(MonthDay other)
+        {
+            return other.Month == Month && other.Day == Day;
+        }
+    }
+}
