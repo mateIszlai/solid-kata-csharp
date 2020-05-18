ae614f99955aa9cb2e56794ef0edd6711fa95215
Mate Iszlai <iszimate@gmail.com>
2020-05-18 11:33:41 +0200

    Add ElectricCar

---
+++ lsp/ElectricCar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lsp
+{
+    public class ElectricCar : Vehicle
+    {
+        private const int BATTERY_FULL = 100;
+
+        public int BatteryLevel { get; private set; }
+
+        public override void fillUpWithFuel()
+        {
+            throw new InvalidOperationException("It's an electric car");
+        }
+
+        public override void chargeBattery()
+        {
+            BatteryLevel = BATTERY_FULL;
+        }
+
+    }
+}
