79f3101eacf43d9c66590ad006f91fe707fecf97
Mate Iszlai <iszimate@gmail.com>
2020-05-18 16:14:28 +0200

    Create Employee class

---
+++ dip/dip/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dip
+{
+    public class Employee
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public string Email { get; private set; }
+
+        Employee(string firstName, string lastName, DateTime dateOfBirth, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Email = email;
+        }
+    }
+}
